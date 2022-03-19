bc2f75df7aeb8e7c5f6b3c213a1038f43262f499
uday.vas <uday.vas@gmail.com>
2022-03-18 18:29:28 -0700

    initial set up

---
+++ src/main/java/com/uvas/example/sbh2/Sbh2Application.java
@@ -0,0 +1,13 @@
+package com.uvas.example.sbh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbh2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbh2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uvas/example/sbh2/Sbh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uvas.example.sbh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbh2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
